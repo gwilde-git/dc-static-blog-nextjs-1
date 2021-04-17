43cb014997fd30aeeabd03ef51c325bdf59960aa
gwilde-git <67952937+gwilde-git@users.noreply.github.com>
2021-04-17 09:23:27 +0100

    Update default.tsx
    service desk blog

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -18,7 +18,7 @@ export default ({ children, contentOnly = false }: DefaultLayoutProps): ReactEle
       <div className="site-container">
         {!contentOnly && (
           <>
-            <Header title="PRODUCT BLOG" />
+            <Header title="SERVICE DESK BLOG" />
           </>
         )}
         <div className="site-content">
